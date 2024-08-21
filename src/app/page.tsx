229533e2dcf96d0b774d65f05624731381df019f
shige <shige992306@gmail.com>
2024-08-21 22:12:09 +0900

    feat: add metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
-export default function Home() {
-  return <div>Home</div>;
-}
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "shige-me",
+  description: "personal site of shige",
+  openGraph: {
+    title: "shige-me",
+    description: "personal site of shige",
+    siteName: "personal site of shige",
+    locale: "jp",
+    type: "website",
+  },
+};
+
+const Home = () => {
+  return <div className="text-[50px]">Home</div>;
+};
+
+export default Home;
