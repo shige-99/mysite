7536ea738d918626730dd182749f9ee40e7601c3
shige <shige992306@gmail.com>
2024-08-22 23:06:28 +0900

    feat: add button link

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,15 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const NotFound = () => {
   return (
-    <div className="container mx-auto py-[50px]">
-      <h2 className="text-[50px] text-center">404</h2>
+    <div className="flex flex-col justify-center items-center py-[100px]">
+      <h2 className="text-[20px] text-center mb-4">
+        ページが見つかりませんでした
+      </h2>
+      <Link href="/">
+        <Button>Back to Home</Button>
+      </Link>
     </div>
   );
 };
