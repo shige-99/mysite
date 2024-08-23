bc4dbaf319079e07643a98d25691a6620af08e17
shige <shige992306@gmail.com>
2024-08-23 22:45:00 +0900

    feat: add copyright

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="text-center opacity-30 text-sm py-4">
+      &copy; {new Date().getFullYear()} shige.
+    </div>
+  );
+};
+
+export default Footer;
