7fd17ab2bfd11577909be50cb1c8e088db58d662
shige <shige992306@gmail.com>
2024-08-24 23:17:28 +0900

    feat: chage button to rounded

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,15 @@
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 const NotFound = () => {
   return (
     <div className="flex flex-col justify-center items-center py-[100px]">
-      <h2 className="text-[20px] text-center mb-4">
+      <h2 className="text-[20px] text-center mb-4 text-black">
         ページが見つかりませんでした
       </h2>
       <Link href="/">
-        <Button>Back to Home</Button>
+        <button className="rounded-full py-2 px-4 text-black bg-white border border-gray-200 hover:bg-slate-50 hover:border-gray-300">
+          ホームに戻る
+        </button>
       </Link>
     </div>
   );
