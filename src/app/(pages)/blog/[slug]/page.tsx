38c6f5b7fd7117fbaf37cd14acede90fe58550b6
shige <shige992306@gmail.com>
2024-08-30 18:36:44 +0900

    style: change mark down format

--- src/app/(pages)/blog/[slug]/page.tsx
+++ src/app/(pages)/blog/[slug]/page.tsx
@@ -13,6 +13,7 @@ interface Params {
 
 interface FrontMatter {
   title: string;
+  date: string;
   [key: string]: any;
 }
 
@@ -22,7 +23,7 @@ export default async function BlogPost({ params }: Params) {
 
   const fileContents = fs.readFileSync(filePath, "utf8");
   const { data, content } = matter(fileContents);
-  const title = (data as FrontMatter).title;
+  const { title, date } = data as FrontMatter;
 
   const processedContent = await unified()
     .use(remarkParse)
@@ -31,13 +32,14 @@ export default async function BlogPost({ params }: Params) {
   const contentHtml = processedContent.toString();
 
   return (
-    <div className="bg-white px-6 py-32 lg:px-8">
+    <div className="bg-white px-6 py-20 lg:px-8">
       <div className="mx-auto max-w-3xl text-base leading-7 text-gray-700">
-        <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-          {title}
-        </h1>
+        <div className="mt-2 text-3xl font-bold py-8 px-4 rounded-lg bg-slate-50 tracking-tight text-gray-900 sm:text-4xl">
+          <h1>{title}</h1>
+          <p className="text-sm text-gray-500 mt-4">{date}</p>
+        </div>
         <div
-          className="mt-6"
+          className="mt-6 prose prose-lg px-4"
           dangerouslySetInnerHTML={{ __html: contentHtml }}
         ></div>
       </div>
