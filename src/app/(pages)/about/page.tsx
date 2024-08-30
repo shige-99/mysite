753507be742aae164da0e21ad7635c386944e9ff
shige <shige992306@gmail.com>
2024-08-30 18:38:39 +0900

    style: change format

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -4,28 +4,19 @@ const AboutPage = () => {
   return (
     <div>
       <Header />
-      <div className="flex flex-col items-center">
-        <h2 className="text-xl font-bold mb-2 underline decoration-black underline-offset-4">
-          Bio
-        </h2>
-        <p>茂太 翼（しげた つばさ）</p>
+
+      <div className="profile-section mb-10 mx-5">
+        <h2 className="text-2xl font-bold mb-4">プロフィール</h2>
+        <p>茂太 翼(しげた つばさ)</p>
         <p>1999.1.14</p>
-        <p>沖縄生まれ、沖縄育ち</p>
-        <p>趣味：野球、旅行、プログラミング</p>
-        <br />
-        <h2 className="text-xl font-bold mb-2 underline decoration-black underline-offset-4">
-          スキル
-        </h2>
+        <p>沖縄生まれ、沖縄育ち。</p>
+        <p>TypeScriptを勉強中の都内SESで勤務するエンジニア。</p>
+        <p>趣味：旅行、野球観戦、プログラミング</p>
+      </div>
+
+      <div className="skill-section mx-5">
+        <h2 className="text-2xl font-bold mb-4">スキル</h2>
         <img src="https://skillicons.dev/icons?i=html,css,tailwindcss,javascript,typescript,nextjs,react,svelte,python,git&perline=5" />
-        <br />
-        <h2 className="text-xl font-bold mb-2 underline decoration-black underline-offset-4">
-          キャリア
-        </h2>
-        <div className="text-left">
-          <p>2023/05~ SES</p>
-          <p>2022/04 セメントメーカー</p>
-          <p>2017/03 琉球大学　卒業</p>
-        </div>
       </div>
     </div>
   );
