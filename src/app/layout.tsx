378a67e9313af177d54256def8b54d458d1116b4
shige <shige992306@gmail.com>
2024-08-31 15:41:55 +0900

    style: remove Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { M_PLUS_Rounded_1c } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 const mplus = M_PLUS_Rounded_1c({
