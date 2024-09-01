98fcb27296e45e4f10eafa9f419c2843a0771e32
shige <shige992306@gmail.com>
2024-09-01 11:03:16 +0900

    style: extends colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        blue: "#eef7ff",
+        black: "#32383E",
+      },
     },
   },
   plugins: [require("@tailwindcss/typography")],
